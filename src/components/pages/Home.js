836be7e02ed03de50adb79efc408d4a7d1ecddbf
arwaalkhathlan <arwa124344@gmail.com>
2024-06-25 10:31:01 +0300

    chore: Update @babel/plugin-proposal-private-property-in-object to version 7.21.11

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="bg-dark text-white vh-100 d-flex justify-content-center align-items-center">
       <div className="justify-content-center align-items-center ">
-        <h1 class="display-5 text-centre  ">Hey</h1>
+        <h1 class="display-5 text-centre ">Hey</h1>
         <h1 class="display-3">My Name is </h1>
         <h1 class="display-1 ">Arwa Alkhathlan</h1>
       </div>
