e43fa00a699d3b940a5aadaa0d409e7e85850ee7
arwaalkhathlan <arwa124344@gmail.com>
2024-06-25 11:52:58 +0300

    added the text in Home.js and included the typewriter library

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,14 +2,46 @@
 
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ReactTyped } from "react-typed";
 
 const Home = () => {
   return (
-    <div className="bg-dark text-white vh-100 d-flex justify-content-center align-items-center">
-      <div className="justify-content-center align-items-center ">
-        <h1 class="display-5 text-centre ">Hey</h1>
-        <h1 class="display-3">My Name is </h1>
-        <h1 class="display-1 ">Arwa Alkhathlan</h1>
+    <div className=" bg-dark text-white vh-100 d-flex justify-content-center align-items-center text-center">
+      <div className="container">
+        <div className="main-text justify-content-center align-items-center ">
+          <p class=" fs-3 opacity-75 ">
+            Welcome to{" "}
+            <ReactTyped
+              strings={["My Website ", "My Portfolio ", "My Page"]}
+              typeSpeed={100}
+              loop
+              backSpeed={20}
+              showCursor={true}
+            />
+          </p>
+
+          <p class="fs-4 opacity-75 ">
+            <ReactTyped
+              strings={[
+                "My Name is Arwa Alkhathlan </br> I'm a passionate programmer, and I'm eager to translate my ideas into real life as best as i could. ",
+              ]}
+              typeSpeed={100}
+              showCursor={true}
+            />
+          </p>
+        </div>
+
+        <div class="p-3">
+          <button type="button" class="btn btn-outline-light btn-lg m-2">
+            Education
+          </button>
+          <button type="button" class="btn btn-outline-light btn-lg m-2">
+            Resume
+          </button>
+          <button type="button" class="btn btn-outline-light btn-lg m-2">
+            Certificates
+          </button>
+        </div>
       </div>
     </div>
   );
